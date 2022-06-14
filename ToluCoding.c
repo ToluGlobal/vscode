2c07d996b8f6402d905e92628ef03b679bf1cf1a
ToluGlobal <101462453+ToluGlobal@users.noreply.github.com>
2022-06-14 12:15:13 -0700

    First Commit

---
+++ ToluCoding.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+        int number = 0;
+        printf("enter a number:\n");
+        scanf("%d",&number);
+
+        if(number==10){
+                printf("number is equal is 10\n");
+}
+
+else if(number==50){
+        printf("number is equal to 50");
+}
+
+else if(number==100){
+        printf("number is equal to 100");
+}
+20
+else{
+  printf("number is not equal to 10,50 or 100");
+}
+
+return 0;
+}
---
+++ product_function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+printf("I am Tolulope Oyejide");
+return 0;
+}
+
+
+
